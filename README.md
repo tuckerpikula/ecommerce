bcb9bae329badbf1191bf3a98ac4cbda3b9a36af
pikulatucker@gmail.com <pikulatucker@gmail.com>
2021-03-24 22:36:07 -0700

    code

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# ecommerce
+# E-Commerce-Back-End
+
+The E-Commerce back end works through postman and you can get, pull, and post data no problem. Since there is no public front end folder, there isn't a screenshot of the application.
---
+++ server.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+
+const express = require('express')
+// import sequelize connection
+const sequelize = require('./config/connection.js')
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use(require('./routes'))
+
+// sync sequelize models to the database, then turn on the server
+sequelize.sync()
+  .then(() => app.listen(process.env.PORT || 3000))
+  .catch(err => console.log(err))
